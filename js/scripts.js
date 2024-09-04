ac9ce19e7903169edf68979dde8bad284fba9c8b
Laraluzz <93933965+Laraluzz@users.noreply.github.com>
2024-09-04 17:45:32 +0100

    Changes in the script
    -Added a function so the menu can appear and disappear in the navigation bar when a click in the hamburguer icon happens
    -When the click happens it also changes the hamburguer to a X icon and vice-versa

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,15 @@
 
-console.log("Hi, I´m Lara, Welcome to my portfolio!");
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    const yi = document.querySelector('.icon i');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+      yi.className='fa-solid fa-x';
+    } else {
+      x.className = 'navtoggle';
+      yi.className='fas fa-bars';
+    }
+  }
+
+
+  
